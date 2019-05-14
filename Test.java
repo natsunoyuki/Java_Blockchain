0cd00be5cc0eba3e48dddcbf377d609d9f7fab43
Yuki Natsume <33706936+natsunoyuki@users.noreply.github.com>
2019-05-14 14:41:50 +0900

    Update Test.java

--- Test.java
+++ Test.java
@@ -8,7 +8,7 @@ public class Test{
     public static void main(String args[]){ 
         BlockChain blockchain = new BlockChain("Test");
 	    
-	int n; //number of leading zeros in nonce
+	int n; //number of leading zeros in hash
     	if(args.length == 0){
     	    System.out.println("Number of leading zeros not set... using default of 4...");
             n = 4; //set default to 4 leading zeros
@@ -16,12 +16,20 @@ public class Test{
     	    n = Integer.parseInt(args[0]);
     	}
 	
-        int M = 100000;
+        int M = 8; //acceptable nonce length
         blockchain.addBlock(createGenesisBlock(n,M));
         System.out.println(blockchain.getLastBlock());
         System.out.println(blockchain);
-		
-        blockchain.addBlock(createNextBlock(n,M,blockchain.getLastBlock(),"Yumi",20));
+	
+        System.out.println();
+	
+        blockchain.addBlock(createNextBlock(n,M,blockchain.getLastBlock(),"Yumi",100));
+        System.out.println(blockchain.getLastBlock());
+        System.out.println(blockchain);
+
+        System.out.println();
+	
+        blockchain.addBlock(createNextBlock(n,M,blockchain.getLastBlock(),"Naomi",100));
         System.out.println(blockchain.getLastBlock());
         System.out.println(blockchain);		
 		
@@ -30,47 +38,50 @@ public class Test{
     //function to create the Genesis block of the block chain.
     //This must be outside the Block class
     public static Block createGenesisBlock(int N, int M) {
-		//create the genesis block of a block chain. This function 
-		//should only be used ONCE!
-		System.out.print("Creating genesis block now... ");
-		int i = 0; //loop counter
-		int init_nonce_length = 1;
-		String init_data_name = "Natsume";
-		int init_data_amount = 100;
-		String zerostring = SHA.ZeroString(N);
-		String init_nonce = RandomString(init_nonce_length);
-		String init_hash = SHA.SHA256(init_nonce);
-		BCData data = new BCData(init_data_name, init_data_amount);
-		Block block = new Block(init_nonce,new Date(),data,init_hash);
-		while(true){
-			if(block.getCurrentHash().substring(0,N).equals(zerostring)){
-			    break;
-			}else{
-		        init_nonce += RandomString(1);
-		        block.updateNonce(init_nonce);
-		        block.updateDate(new Date());
-		        block.updateHash();
-		        i++;
-			if(i%M==0){
-			    //as we do not want too long a nonce, reset the length
-			    //to 1 after a certain number of trials
-                            System.out.printf("\n%d iterations complete...", i);
-                            init_nonce = RandomString(init_nonce_length);
-                            block.updateNonce(init_nonce);
-                            block.updateDate(new Date());
-                            block.updateHash();
-                            i=1;
-                        }		
-		    }
-		}
-        System.out.printf("%d iterations to create genesis block.\n",i);
+        //create the genesis block of a block chain. This function 
+        //should only be used ONCE!
+        System.out.print("Creating genesis block now... ");
+        int i = 1; //loop counter
+        int counter = 0;
+        int init_nonce_length = 1;
+        String init_data_name = "Natsume";
+        int init_data_amount = 100;
+        String zerostring = SHA.ZeroString(N);
+        String init_nonce = RandomString(init_nonce_length);
+        String init_hash = SHA.SHA256(init_nonce);
+        BCData data = new BCData(init_data_name, init_data_amount);
+        Block block = new Block(init_nonce,new Date(),data,init_hash);
+        while(true){
+            counter++;
+            if(block.getCurrentHash().substring(0,N).equals(zerostring)){
+                break;
+            }else{
+                init_nonce += RandomString(1);
+                block.updateNonce(init_nonce);
+                block.updateDate(new Date());
+                block.updateHash();
+                i++;
+                if(i%M==0){
+                    //as we do not want too long a nonce, reset the length
+                    //to 1 after a certain number of trials
+                    //System.out.printf("\n%d iterations complete...", i);
+                    init_nonce = RandomString(init_nonce_length);
+                    block.updateNonce(init_nonce);
+                    block.updateDate(new Date());
+                    block.updateHash();
+                    i=1;
+               }		
+           }
+        }
+        System.out.printf("%d iterations to create genesis block.\n",counter);
 		return block;
     }
     
     public static Block createNextBlock(int N, int M, Block prevBlock, String init_data_name, int init_data_amount) {
 		//using data from the previous block, create a new block
 		System.out.print("Creating new block now... ");
-		int i = 0; //loop counter
+		int i = 1; //loop counter
+                                    int counter = 0;
 		int init_nonce_length = 1;
 		String zerostring = SHA.ZeroString(N);
 		String init_nonce = RandomString(init_nonce_length);
@@ -79,6 +90,7 @@ public class Test{
 		Block block = new Block(init_nonce,new Date(),data,init_hash);
         
 		while(true){
+                        counter++;
 			if(block.getCurrentHash().substring(0,N).equals(zerostring)){
 			    break;
 			}else{
@@ -90,7 +102,7 @@ public class Test{
 			if(i%M==0){
 			    //as we do not want too long a nonce, reset the length
 			    //to 1 after a certain number of trials
-                            System.out.printf("\n%d iterations complete...", i);
+                            //System.out.printf("\n%d iterations complete...", i);
                             init_nonce = RandomString(init_nonce_length);
                             block.updateNonce(init_nonce);
                             block.updateDate(new Date());
@@ -99,14 +111,15 @@ public class Test{
                         }
 		    }
 		}
-            System.out.printf("%d iterations to create new block.\n",i);
+            System.out.printf("%d iterations to create new block.\n",counter);
             return block;		
 	}
     
     //creates a repeating random alpha-numeric string of length N
     public static String RandomString(int N){
-        String a = "abcdefghijklmnopqrstuvwxyz0123456789";
-        int alen = 36;
+        //String a = "abcdefghijklmnopqrstuvwxyz0123456789";
+        String a = "0123456789abcdef";
+        int alen = a.length();
         String string = "";
         int b;
         for(int i=0;i<N;i++){
@@ -116,5 +129,3 @@ public class Test{
         return string;
     }    
 }
-
-
